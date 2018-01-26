716e0a8bb20e14c0be07f41c6a11439a688ff313
sea791748235 <sea791748235@outlook.com>
2018-01-26 15:45:33 +0800

    Initial commit

---
+++ src/main/java/com/sea/test/Test.java
@@ -0,0 +1,13 @@
+/**
+ * FileName:    Test
+ * Author:      sealllsea
+ * Date:        2018/1/26 13:19
+ * Description: test
+ */
+package com.sea.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("AAA");
+    }
+}
